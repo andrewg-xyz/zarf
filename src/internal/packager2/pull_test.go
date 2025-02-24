7a3296c1898d35d02c3fb5a955bc25aa3a4c5d7d
Austin Abro <AustinAbro321@gmail.com>
2025-02-24 20:06:29 +0000

    fix shasum
    Signed-off-by: Austin Abro <AustinAbro321@gmail.com>

--- src/internal/packager2/pull_test.go
+++ src/internal/packager2/pull_test.go
@@ -38,7 +38,7 @@ func TestPull(t *testing.T) {
 	})
 
 	dir := t.TempDir()
-	shasum := "bef73d652f004d214d5cf9e00195293f7ae8390b8ff6ed45e39c2c9eb622b873"
+	shasum := "f9b15b1bc0f760a87bad68196b339a8ce8330e3a0241191a826a8962a88061f1"
 	err := Pull(ctx, srv.URL, dir, shasum, "amd64", filters.Empty(), "", false)
 	require.NoError(t, err)
 
